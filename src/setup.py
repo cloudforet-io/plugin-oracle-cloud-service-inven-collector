f6ad847a1091ed91460db78d00dadaa991b408ec
Sooyoung98 <sooyoungkim@mz.co.kr>
2024-01-06 16:07:42 +0900

    fix: modified parts of code related to docker and versioning

--- src/setup.py
+++ src/setup.py
@@ -16,26 +16,20 @@
 
 from setuptools import setup, find_packages
 
-with open('VERSION', 'r') as f:
+with open("VERSION", "r") as f:
     VERSION = f.read().strip()
     f.close()
 
 setup(
-    name='plugin-oracle-cloud-services',
+    name="plugin-oracle-cloud-services",
     version=VERSION,
-    description='Oracle Cloud Infrastructure cloud service inventory collector',
-    long_description='',
-    url='https://www.spaceone.dev/',
-    author='MEGAZONE SpaceONE Team',
-    author_email='admin@spaceone.dev',
-    license='Apache License 2.0',
+    description="Oracle Cloud Infrastructure cloud service inventory collector",
+    long_description="",
+    url="https://www.spaceone.dev/",
+    author="MEGAZONE SpaceONE Team",
+    author_email="admin@spaceone.dev",
+    license="Apache License 2.0",
     packages=find_packages(),
-    install_requires=[
-        'spaceone-core',
-        'spaceone-api',
-        'spaceone-tester',
-        'schematics',
-        'oci'
-    ],
+    install_requires=["spaceone-api", "schematics", "oci"],
     zip_safe=False,
 )
